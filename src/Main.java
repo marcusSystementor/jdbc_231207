0ffc7523d6000638fcaefc339ba590fdc0f10d1f
Marcus B <marcus.brederfalt@systementor.se>
2023-12-07 10:50:20 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import Model.Category;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DBConnection dbConnection = new DBConnection();
+        dbConnection.open();
+
+        System.out.println(dbConnection.queryCategories().size());
+
+        ArrayList<Category> categories = dbConnection.queryCategories();
+
+        for (Category katt: categories) {
+            System.out.println(katt.toString());
+        }
+
+
+    }
+
+}
